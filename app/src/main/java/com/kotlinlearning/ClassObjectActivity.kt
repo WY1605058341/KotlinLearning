1acd1c36afae601c67ab800068921be5c643a23e
weiyu <hkx@weiyudeMacBook-Pro.local>
2021-01-19 16:18:35 +0800

    学习

--- app/src/main/java/com/kotlinlearning/ClassObjectActivity.kt
+++ app/src/main/java/com/kotlinlearning/ClassObjectActivity.kt
@@ -2,26 +2,56 @@ package com.kotlinlearning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ClassObjectActivity : AppCompatActivity() {
+
+    lateinit var tv: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_class_object)
 
+        tv = findViewById(R.id.tv)
+
+
         //我们可以像使用普通函数那样使用构造函数创建类实例：
         val site = Runoob()
         //使用类属性
-        site.city
-        site.name
+        var mycity = site.city
+        var myName = site.name
+
+        tv.text = "city=$mycity \n name=$myName" //换行显示城市和名称
+
+//        print("city=$mycity")
+//
+//        print("name=$myName")
+
+
+        var person: Person = Person()
+        person.lastName = "wang"
+        tv.text = tv.text as String + "\n lastName:${person.lastName}"
+
+        person.no = 9
+        tv.text = tv.text as String + "\n no:${person.no}"
+
+        person.no = 20
+        tv.text = tv.text as String + "\n no:${person.no}"
 
 
     }
 
 
-    class Runoob {
+    class Runoob { //定义Runoob 类
         var name: String = "张三"
         var url: String = "https://www.baidu.com"
         var city: String = "常州"
+
+        fun foo() { //成员函数
+            print("Foo 成员函数")
+        }
+
+
     }
 
 
--- app/src/main/java/com/kotlinlearning/MainActivity.kt
+++ app/src/main/java/com/kotlinlearning/MainActivity.kt
@@ -11,26 +11,38 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btn1: Button
     private lateinit var btn2: Button
     private lateinit var btn3: Button
+    private lateinit var btn4: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //函数声明
         btn1 = findViewById(R.id.btn_1)
         btn1.setOnClickListener(View.OnClickListener {
             val intent = Intent(this@MainActivity, FunActivity::class.java)
             startActivity(intent)
         })
 
+        //条件控制
         btn2 = findViewById(R.id.btn_2)
         btn2.setOnClickListener(View.OnClickListener {
             val intent = Intent(this@MainActivity, ConditionActivity::class.java)
             startActivity(intent)
         })
 
+        //循环控制
         btn3 = findViewById(R.id.btn_3)
         btn3.setOnClickListener(View.OnClickListener {
             val intent = Intent(this@MainActivity, LoopActivity::class.java)
+            startActivity(intent)
+        })
+
+        //类与对象
+        btn4 = findViewById(R.id.btn_4)
+        btn4.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this@MainActivity, ClassObjectActivity::class.java)
             startActivity(intent)
         })
 
